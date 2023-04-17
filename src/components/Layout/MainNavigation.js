923c9d89ac6f3215edc0113851dba2f938f8a011
AbhinasDash <abhinasdash143@gmail.com>
2023-04-17 07:59:55 +0530

    Implementing Logout functionality

--- src/components/Layout/MainNavigation.js
+++ src/components/Layout/MainNavigation.js
@@ -7,6 +7,10 @@ import AuthContext from "../../store/AuthContext";
 const MainNavigation = () => {
   const authCtx = useContext(AuthContext);
   const isLoggedIn = authCtx.isLogin;
+
+  const logoutHandler = () => {
+    authCtx.logout()
+  };
   return (
     <header className={classes.header}>
       <Link to="/">
@@ -26,7 +30,7 @@ const MainNavigation = () => {
           )}
           {isLoggedIn && (
             <li>
-              <button>Logout</button>
+              <button onClick={logoutHandler}>Logout</button>
             </li>
           )}
         </ul>
